fed4fa5ffbdf4ab729b8f6cf6be6d9d0af3eee01
Malinda Mosholder <m.m.mosholder@gmail.com>
2018-08-05 15:40:33 -0600

    testing

--- resources/assets/js/pages/SignUp.vue
+++ resources/assets/js/pages/SignUp.vue
@@ -5,6 +5,7 @@
         <div class="row">
           <form class="col-12 col-sm-8 offset-sm-2">
             <div class="row">
+              <div class="col-12">Testing</div>
               <div class="col-12 form-group">
                 <label for="name">Name *</label>
                 <input class="text-input" type="text" name="name" v-model="newUser.name" placeholder="First Last Name">
--- webpack.mix.js
+++ webpack.mix.js
@@ -52,3 +52,11 @@ mix.js("resources/assets/js/app.js", "public/js").sass(
     "resources/assets/sass/app.scss",
     "public/css"
 );
+
+if (!mix.inProduction()) {
+  mix.sourceMaps();
+}
+
+if (mix.inProduction()) {
+  mix.version();
+}
